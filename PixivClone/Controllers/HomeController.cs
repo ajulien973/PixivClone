848b46ea794c85355b490fe44248fdcab3c182bc
ekanseht <alexis.julien@edu.esiee.fr>
2014-07-18 11:21:23 +0100

    Added IOC Container Ninject

--- PixivClone/Controllers/HomeController.cs
+++ PixivClone/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PixivClone.Models;
 
 namespace PixivClone.Controllers
 {
     public class HomeController : Controller, IHomeController
     {
+        private IRepository<User> _repo;
+
+        public HomeController(IRepository<User> repo)
+        {
+            _repo = repo;
+        }
+
+
         public ActionResult Index()
         {
             return View();
--- PixivClone/Models/AccountModels.cs
+++ PixivClone/Models/AccountModels.cs
@@ -9,6 +9,8 @@ namespace PixivClone.Models
     {
         public Guid UserId { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 
 
